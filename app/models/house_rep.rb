6cd70b8fdc6976928587352bb056ea97a728af0a
Danny Castillo <danny.webgraphics@gmail.com>
2017-06-04 17:17:32 -0600

    Add as rep methods to senate house reps models

--- app/models/house_rep.rb
+++ app/models/house_rep.rb
@@ -1,3 +1,20 @@
 class HouseRep < ActiveRecord::Base
   belongs_to :district
+
+  def as_representative
+    {
+      first_name: first_name,
+      middle_name: middle_name,
+      last_name: last_name,
+      gender: gender,
+      party: party,
+      elected_date: elected_date,
+      phone: phone,
+      website_url: website_url,
+      facebook_account: facebook_account,
+      twitter_account: twitter_account,
+      dob: dob,
+      term_end_date: term_end_date
+    }
+  end
 end
--- app/models/senate_rep.rb
+++ app/models/senate_rep.rb
@@ -1,3 +1,20 @@
 class SenateRep < ActiveRecord::Base
   belongs_to :state
+
+  def as_representative
+    {
+      first_name: first_name,
+      middle_name: middle_name,
+      last_name: last_name,
+      gender: gender,
+      party: party,
+      elected_date: elected_date,
+      phone: phone,
+      website_url: website_url,
+      facebook_account: facebook_account,
+      twitter_account: twitter_account,
+      dob: dob,
+      term_end_date: term_end_date
+    }
+  end
 end
