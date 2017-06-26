829e7747348cff6fde843480559cb90f13317b8b
Danny Castillo <danny.webgraphics@gmail.com>
2017-06-25 22:44:56 -0600

    Update rep models to match column updates

--- app/models/house_rep.rb
+++ app/models/house_rep.rb
@@ -7,14 +7,16 @@ class HouseRep < ActiveRecord::Base
       middle_name: middle_name,
       last_name: last_name,
       gender: gender,
+      dob: dob,
       party: party,
-      elected_date: first_elected_date,
       phone: phone,
       website_url: website_url,
       facebook_account: facebook_account,
       twitter_account: twitter_account,
-      dob: dob,
-      term_end_date: term_end_date
+      term_end_date: term_end_date,
+      began_office_year: began_office_at.year,
+      ended_office_year: ended_office_at.nil? ? nil : ended_office_at.year,
+      reelection_year: reelection_date.year
     }
   end
 end
