44dc838ae3cd4b4aeea452a8eb42adbc2d952aab
Danny Castillo <danny.webgraphics@gmail.com>
2017-06-30 07:53:46 -0600

    Add controllers/views for reps in dashboard

---
+++ app/controllers/dashboard/house_reps_controller.rb
@@ -0,0 +1,19 @@
+module Dashboard
+  class HouseRepsController < DashboardController 
+
+    def index
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+
+  end
+end
---
+++ app/controllers/dashboard/senate_reps_controller.rb
@@ -0,0 +1,19 @@
+module Dashboard
+  class SenateRepsController < DashboardController 
+
+    def index
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+
+  end
+end
